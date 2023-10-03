f81683afefd9d4cfc51b7001e2232c374316ad38
Frank Blau <fjblau@gmail.com>
2023-10-03 20:15:59 +0200

    web_summary update

--- chapter_06.py
+++ chapter_06.py
@@ -9,7 +9,7 @@ from langchain.schema import (
 from langchain.callbacks import get_openai_callback
 
 import requests
-from bs4 import BeautifulSoup
+from beautifulsoup4 import BeautifulSoup
 from urllib.parse import urlparse
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
